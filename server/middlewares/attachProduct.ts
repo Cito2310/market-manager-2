40edd91d8daeae42a02aa618d131caee25206a9b
Cito2310 <cito2310@gmail.com>
2025-10-24 15:00:26 -0300

    added middlewares updateProduct

---
+++ server/middlewares/attachProduct.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { Product } from "../apis/product/productModels";
+
+export const attachProduct = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const id = req.params!.id;
+    const product = await Product.findById(id);
+
+    if (!req.body) req.body = {};
+    req.body.product = product;
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
