3d15decb971d932b3e4546962bcafc8284a5efaf
Cito2310 <cito2310@gmail.com>
2025-10-25 00:23:31 -0300

    added server and release 2.0.0

--- server/server.ts
+++ server/server.ts
@@ -2,12 +2,16 @@ import express from "express";
 import cors from "cors";
 
 import { dbConnection } from './database/config';
-import { routeUser } from './users_api/userRoutes';
+import { routeProduct } from "./apis/product/routes/routesProduct";
+import { routeUser } from "./apis/user/routes/routesUser";
+import { routeCategory } from "./apis/category/routes/routeCategory";
 
 export class Server {
     private app = express()
     private paths = {
         user : "/api/user",
+        product : "/api/product",
+        category : "/api/category",
     }
 
     constructor(){
@@ -18,6 +22,8 @@ export class Server {
 
     private routes() {
         this.app.use( this.paths.user, routeUser )
+        this.app.use( this.paths.product, routeProduct )
+        this.app.use( this.paths.category, routeCategory )
     }
 
     private connectDB() {dbConnection()}
